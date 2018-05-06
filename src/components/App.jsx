b39c9cb16d29990983f2c8ba549356c9f4f0b91d
ACODE91 <aaron.z.lao@gmail.com>
2018-05-05 21:30:56 -0700

    added image for css drop down menu category button

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
+import React from 'react';
 import MenuBox from './MenuBox.jsx'
 import BillTab from './BillTab.jsx'
 import axios from 'axios';
 import style from './App.css';
 
-class App extends Component {
+class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
@@ -12,15 +12,39 @@ class App extends Component {
       appetizersDisplay: false,
       mainsDisplay: false,
       beveragesDisplay: false,
+      extrasDisplay: false,
       appetizerData: [],
       mainData: [],
       beverageData: [],
-      extraFoodData: []
+      extraFoodData: [],
     }
   }
 
   componentDidMount() {
+    const appState = this.state;
+    const endpoint = window.location.pathname.slice(3);
+
+    axios.get(`/data/${endpoint}/extra`, {
+    }).then(function(response) {
+      appState.extraFoodData = response.data;
+    });
+
+    axios.get(`/data/${endpoint}/main`, {
+    }).then(function(response) {
+      appState.mainData = response.data;
+    });
+
+    axios.get(`/data/${endpoint}/beverage`, {
+    }).then(function(response) {
+      appState.beverageData = response.data;
+    });
+
+    axios.get(`/data/${endpoint}/appetizer`, {
+    }).then(function(response) {
+      appState.appetizerData = response.data;
+    });
     
+    this.forceUpdate();
   }
 
   handleClick() {
@@ -31,11 +55,51 @@ class App extends Component {
     }
   }
 
+  handleMenuCategoryClick(e) {
+    if(e.currentTarget.value === 'Appetizer' && !this.state.appetizersDisplay) {
+      this.setState({appetizersDisplay: true, mainsDisplay: false});
+    } else if(this.state.appetizersDisplay) {
+      this.setState({appetizersDisplay: false});
+    }
+
+    if(e.currentTarget.value === 'Mains' && !this.state.mainsDisplay) {
+      this.setState({mainsDisplay: true, appetizersDisplay: false, beveragesDisplay: false, extrasDisplay: false});
+    } else if(this.state.mainsDisplay) {
+      this.setState({mainsDisplay: false});
+    }
+
+    if(e.currentTarget.value === 'Mains' && !this.state.mainsDisplay) {
+      this.setState({mainsDisplay: true, appetizersDisplay: false, beveragesDisplay: false, extrasDisplay: false});
+    } else if(this.state.mainsDisplay) {
+      this.setState({mainsDisplay: false});
+    }
+    
+    if(e.currentTarget.value === 'Beverages' && !this.state.beveragesDisplay) {
+      this.setState({beveragesDisplay: true, appetizersDisplay: false, mainsDisplay: false, extrasDisplay: false});
+    } else if(this.state.beveragesDisplay) {
+      this.setState({beveragesDisplay: false});
+    }
+
+    if(e.currentTarget.value === 'Extras' && !this.state.extrasDisplay) {
+      this.setState({extrasDisplay: true, appetizersDisplay: false, mainsDisplay: false, beveragesDisplay: false});
+    } else if(this.state.extrasDisplay) {
+      this.setState({extrasDisplay: false});
+    }
+    console.log(this.state)
+  }
+
   render() {
     return (
       <div className="App">
-      <button onClick={this.handleClick.bind(this)}>Display Menu</button>
-      {(this.state.display) ? <MenuBox /> : <div></div>}
+      <button className="display-menu-button" onClick={this.handleClick.bind(this)}>Menu</button>
+      {(this.state.display) ? <MenuBox
+      menuCategoryClick={this.handleMenuCategoryClick.bind(this)}
+      appState={this.state} 
+      appetizerProp={this.state.appetizerData}
+      mainProp={this.state.mainData}
+      extraProp={this.state.extraFoodData}
+      beverageProp={this.state.beverageData}
+      /> : <div></div>}
       </div>
     );
   }
